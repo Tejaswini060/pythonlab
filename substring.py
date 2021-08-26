d9992bf35e95efa0605f3ce309cedffe29f52b70
Tejaswini060 <79572948+Tejaswini060@users.noreply.github.com>
2021-08-26 10:57:00 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,6 @@
+string=input("enter a string")
+sub =input("enter substring")
+if(string.find(sub)== -1):
+    print("substring is not found in a given string")
+else:
+    print("substring is found in a given string")
