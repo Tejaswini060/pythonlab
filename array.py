f0d8f6071139f43a4795465a2a0a8cdec27fac24
Tejaswini060 <79572948+Tejaswini060@users.noreply.github.com>
2021-09-22 14:06:07 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,15 @@
+def rotate arr (a,d):
+    temp =[]
+    n=len(a)
+    for i in range(d,n):
+        temp.append(a[i])
+        i=0
+        for i in range(0,0):
+            temp.append(a[i])
+            a=temp.copy()
+            return a
+        arr=[1,2,3,4,5,6,7]
+        print("array after left rotation is:",end:' ')
+        print(rotate array (arr,2)
+              
+
